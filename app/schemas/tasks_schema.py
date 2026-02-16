3b29e4b27e3ce187bffddff45ae7cb45725fa137
shahbaz alam <sbzalam2024@gmail.com>
2026-02-17 00:35:42 +0530

    deleted useless App folder and added project tree in json

--- app/schemas/tasks_schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Tasks(BaseModel):
-    name: str = Field(min_length=3)
-    discriptions:str = Field(min_length=3)
-    date:str 
--- app/services/task_service.py
+++
@@ -1,7 +0,0 @@
-from app.schemas.tasks_schema import Tasks
-
-
-def tasks_states(task:Tasks):
-    pass
-def new_task(task:Tasks):
-    return "task created"
